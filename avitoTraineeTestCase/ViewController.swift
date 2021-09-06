c26b0d8f18dff6b10b2029585d72a858c47c24f7
mikesemenov <mikesemenov.ru@gmail.com>
2021-09-06 14:06:07 +0300

    Initial Commit

---
+++ avitoTraineeTestCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avitoTraineeTestCase
+//
+//  Created by Mikhail Semenov on 06.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
